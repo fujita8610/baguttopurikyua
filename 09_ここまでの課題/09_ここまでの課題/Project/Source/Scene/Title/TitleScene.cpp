56c0eb70ff45603eac8f845d15604efe6fee87c0
Yuunosuke Kai <yuuno403@icloud.com>
2026-02-05 10:09:14 +0900

    Titleかきました

--- 09_ここまでの課題/09_ここまでの課題/Project/Source/Scene/Title/TitleScene.cpp
+++ 09_ここまでの課題/09_ここまでの課題/Project/Source/Scene/Title/TitleScene.cpp
@@ -4,6 +4,7 @@
 
 int g_TitleBGHandle = -1;
 int g_TitleTextHandle = -1;
+int g_TitleOptionHandle = -1;
 
 void InitTitleScene()
 {
@@ -13,6 +14,7 @@ void LoadTitleScene()
 {
 	g_TitleBGHandle = LoadGraph("Data/Title/BG.png");
 	g_TitleTextHandle = LoadGraph("Data/Title/TitleText.png");
+	g_TitleOptionHandle = LoadGraph("Data/Title/TitleOption.png");
 }
 
 void StartTitleScene()
@@ -39,6 +41,7 @@ void DrawTitleScene()
 {
 	DrawGraph(0, 0, g_TitleBGHandle, TRUE);
 	DrawGraph(200, 200, g_TitleTextHandle, TRUE);
+	DrawGraph(200, 600, g_TitleOptionHandle, TRUE);
 }
 
 void FinTitleScene()
