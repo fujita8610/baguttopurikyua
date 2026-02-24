6ce325aad4a0617702e903ea630da06a878adab0
nakayamaharuki0724 <nakayamaharuki2006724@gmail.com>
2026-02-24 11:13:20 +0900

    SE

---
+++ Super Jump Action/Source/scene/sceneManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "scene.h"
+
+
+
+class SceneManager
+{
+public:
+
+    static void ChangeScene(Scene* scene);
+
+    static void Update();
+
+    static void Draw();
+
+
+private:
+
+    static Scene* currentScene;
+
+};
